e003a10525a9fb912b6fb74a80f048f8a68fd51a
DARIO SULCA <dario.martin.sulca@gmail.com>
2022-10-17 16:48:33 -0300

    termine el test 03

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,53 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    
+    var acc = 0
+    for (var i = 1; i <=10 ; i++) {
+      acc = acc + i ;
+    }
+    
+    return acc
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  var nuevoArray = []
+  var indice = 0
+  while (indice < array.length) {  
+    if (array[indice]%2 === 0){
+      nuevoArray.push(array[indice])
+    } 
+    indice++;
+  }      
+    return nuevoArray  
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  var Cuadrado = array.map(function(elemento){
+    return elemento ** 2  }  )
+    return Cuadrado
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  var suma = array.reduce (function (acc,elemento){
+    return acc + elemento  }, 0)
+    return suma
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  var numeroAString = num + '';
+  return numeroAString.length  
 }
   
   
---
+++ ejemplo.js
@@ -0,0 +1,13 @@
+function imprimirSumaNumeros() {
+    // Imprime la suma de los números del 1 al 10
+    // Pista: usa un acumulador
+    // Tu código:
+    
+  
+    var suma = 0
+    while(n < 11) {
+      n++;
+      suma = suma + n
+    }
+    return suma
+}
